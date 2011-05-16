d9d51b82993c5992bc2338cc33eb446ab9d11196
Thibault Porteboeuf <thibault.porteboeuf@telecom-paristech.fr>
2011-05-16 17:36:08 +0200

    Compilation ok

--- SoC/SoC/video_in/video_in.cpp
+++ SoC/SoC/video_in/video_in.cpp
@@ -3,25 +3,32 @@
  *
  *       Filename:  video_in.cpp
  *
- *    Description:  video_in module
- *		    based on the generic wb master module
+ *    Description:  WB master video in
  *
- *         Author:  Thibault Porteboeuf, thibault.porteboeuf@telecom-paristech.fr
- *	   Credit:  Tarik Graba (TG), tarik.graba@telecom-paristech.fr
+ *         Author:  Tarik Graba (TG), tarik.graba@telecom-paristech.fr
  *        Company:  Telecom Paris TECH
  *
  * =============================================================================
  */
 
-#include <systemc>
+
+#include <cstdio>
+
+#include <fstream>
+#include <sstream>
+#include <iomanip>
 
 #include "video_in.h"
 #include <video_gen.h>
 #include "segmentation.h"
 
+
 #define tmpl(x) template<typename wb_param> x VideoInModule<wb_param>
 
+
+
 namespace soclib { namespace caba {
+
 
 	template<typename wb_param>
 	 void VideoInModule<wb_param>::clk_pix_event() {
@@ -31,13 +38,9 @@ namespace soclib { namespace caba {
 			end_frame=true;
 		}	
 		else if (line_valid) {
-			Buffer[pos] = pixel_in.read();
-			pos++;
-			pos=pos%BUFFER_SIZE;
-			long delta = pos-offset;
-			if (delta<0) {delta+=BUFFER_SIZE;}
+			pix_fifo->write(pixel_in.read());
 
-			if (2*delta>=BUFFER_SIZE) {
+			if (2*pix_fifo->num_available()>=BUFFER_SIZE) {
 				//ACTIVATE WRITE SEQUENCE
 				write=true;
 			}
@@ -49,167 +52,58 @@ namespace soclib { namespace caba {
 	void VideoInModule<wb_param>::clk_wb_event() {
 		if (write) {
 			write=false;
-			//CREATE A LOCAL COPY OF THE DATA TO BE TRANSFERED
 			unsigned char local_buffer[BUFFER_SIZE];
-			long p = offset;
 			long idx = 0;
-			long delta = p - pos;
-			if (delta<0) {delta+=BUFFER_SIZE;}
-			while (delta<4) {
+			//CREATE A LOCAL COPY OF THE DATA TO BE TRANSFERED
+			while (pix_fifo->num_available()>4) {
 				for (int i=0; i<4; i++) {
-					local_buffer[idx] = Buffer[p];
-					idx++;
-					p++;
-					p=p%BUFFER_SIZE;
+					local_buffer[idx+i] = pix_fifo->read();
 				}
-				delta = p - pos;
-				if (delta<0) {delta+=BUFFER_SIZE;}
+				idx+=4;
 			}
-			//FREEING SOME SPACE INSIDE THE SHARED BUFFER
-			offset+=idx;
-			offset=offset%BUFFER_SIZE;
 			
-			wb_write_blk(RAM_BASE,(uint32_t*) local_buffer,idx/4);
+			master0.wb_write_blk(RAM_BASE,(uint32_t*) local_buffer, idx/4);
 		}
 	}
 
-    ////////////////////////////////////////////////////////////////////////////
-    // single read/write operation
-    ////////////////////////////////////////////////////////////////////////////
 
-    template<typename wb_param>
-        uint32_t VideoInModule<wb_param>::wb_read_at
-        ( uint32_t addr )
-        {
-            sc_core::wait(p_clk.negedge_event());
-            p_wb.ADR_O = addr;
-            p_wb.SEL_O = 0xF;
-            p_wb.STB_O = true;
-            p_wb.CYC_O = true;
-            p_wb.WE_O  = false;
 
-            // sc_core::wait for ack
-            WaitWbAck();
-            // clean request after clk falling edge
-            sc_core::wait(p_clk.negedge_event());
-            CleanWb();
-            num_reads++;
-            return (uint32_t) p_wb.DAT_I.read();
+    tmpl(/**/)::VideoInModule (sc_module_name name )
+        :sc_core::sc_module(name), p_clk("p_clk"),p_resetn("p_resetn"), master0(p_clk,p_wb)
+    {
+	pix_fifo = new sc_fifo<unsigned char>(BUFFER_SIZE);
+	
+        // sc thread
+        SC_THREAD(clk_pix_event);
+	sensitive << p_clk_pix.pos();
+	dont_initialize();
+	
+	SC_THREAD(clk_wb_event);
+	sensitive << p_clk.pos();
+	dont_initialize();
+        // no sensitivity list, waits have to be explicit
+    }
+
+
+    // wait cycles
+    tmpl(void):: wait_cycles (uint32_t delay)
+    {
+#ifdef SOCLIB_MODULE_DEBUG
+        std::cout
+            << name()
+            << " wait start @" << sc_time_stamp()
+            << std::endl;
+#endif
+        while (delay--) {
+            wait(p_clk.posedge_event());
         }
+#ifdef SOCLIB_MODULE_DEBUG
+        std::cout
+            << name()
+            << " wait end @" << sc_time_stamp()
+            << std::endl;
+#endif
+    }
 
-    template<typename wb_param>
-        void     VideoInModule<wb_param>::wb_write_at
-        (uint32_t addr, uint8_t mask, uint32_t data)
-        {
-            // set request on clk falling edge
-            sc_core::wait(p_clk.negedge_event());
-            p_wb.DAT_O = data;
-            p_wb.ADR_O = addr;
-            p_wb.SEL_O = mask;
-            p_wb.STB_O = true;
-            p_wb.CYC_O = true;
-            p_wb.WE_O  = true;
-
-            // sc_core::wait for ack
-            WaitWbAck();
-            // clean request after clk falling edge
-            sc_core::wait(p_clk.negedge_event());
-            CleanWb();
-            num_writes++;
-        }
-
-    ////////////////////////////////////////////////////////////////////////////
-    // burst read/write operation to/from an uint32_t array
-    ////////////////////////////////////////////////////////////////////////////
-
-    template<typename wb_param>
-        void VideoInModule<wb_param>::wb_read_blk
-        ( uint32_t saddr, uint32_t num, uint32_t *dest)
-        {
-            for (uint32_t i = 0; i< num; i++)
-            {
-                sc_core::wait(p_clk.negedge_event());
-                p_wb.ADR_O = saddr;
-                p_wb.SEL_O = 0xF;
-                p_wb.STB_O = true;
-                p_wb.CYC_O = true;
-                p_wb.WE_O  = false;
-
-                // sc_core::wait for ack
-                WaitWbAck();
-                num_reads++;
-                *dest++ = (uint32_t) p_wb.DAT_I.read();
-                // next data
-                saddr = saddr + 4;
-            }
-            // clean request after clk falling edge
-            sc_core::wait(p_clk.negedge_event());
-            CleanWb();
-        }
-
-    template<typename wb_param>
-        void     VideoInModule<wb_param>::wb_write_blk
-        ( uint32_t saddr, uint32_t *data, uint32_t num)
-        {
-            for (uint32_t i = 0; i< num; i++)
-            {
-                // set request on clk falling edge
-                sc_core::wait(p_clk.negedge_event());
-                p_wb.DAT_O = *data++;
-                p_wb.ADR_O = saddr;
-                p_wb.SEL_O = 0xFF;
-                p_wb.STB_O = true;
-                p_wb.CYC_O = true;
-                p_wb.WE_O  = true;
-
-                // sc_core::wait for ack
-                WaitWbAck();
-                num_writes++;
-                // next data
-                saddr = saddr + 4;
-            }
-            // clean request after clk falling edge
-            sc_core::wait(p_clk.negedge_event());
-            CleanWb();
-        }
-
-    // Reset
-    template<typename wb_param>
-        void     VideoInModule<wb_param>:: reset()
-        {
-            trans_wait_cycles    = 0;
-            num_writes           = 0;
-            num_reads            = 0;
-            CleanWb();
-        }
-
-    // print simulation statistics
-    template<typename wb_param>
-        void     VideoInModule<wb_param>:: print_stats()
-        {
-            std::cout << *this << std::endl;
-        }
-
-    ////////////////////////////////////////////////////////////////////////////
-    //  Constructor
-    ////////////////////////////////////////////////////////////////////////////
-
-    template<typename wb_param>
-        VideoInModule<wb_param>::VideoInModule ( 
-                sc_core::sc_in<bool> &p_clk,
-                soclib::caba::WbMaster<wb_param> &p_wb
-                ): p_clk(p_clk),p_wb(p_wb)
-        {
-		this->p_clk	(p_clk);
-		this->p_wb	(p_wb);
-
-		SC_THREAD(clk_pix_event);
-		sensitive << clk_pix.pos();
-		dont_initialize();
-
-		SC_THREAD(clk_wb_event);
-		sensitive << p_clk;
-		dont_initialize();
-        }
-}}
+}}// namespace
 
--- SoC/SoC/video_in/video_in.h
+++ SoC/SoC/video_in/video_in.h
@@ -3,166 +3,62 @@
  *
  *       Filename:  video_in.h
  *
- *    Description:  video_in module
- *		    based on the generic wb master module
+ *    Description:  video in master
  *
- *         Author:  Thibault Porteboeuf, thibault.porteboeuf@telecom-paristech.fr
- *	   Credit:  Tarik Graba (TG), tarik.graba@telecom-paristech.fr
+ *         Author:  Tarik Graba (TG), tarik.graba@telecom-paristech.fr
  *        Company:  Telecom Paris TECH
  *
  * =============================================================================
  */
 
+#ifndef __VIDEO_IN__
+#define __VIDEO_IN__
 
-#ifndef _VIDEO_IN_MODULE_
-#define _VIDEO_IN_MODULE_
-
-
-#include <iostream>
 
 #include <systemc>
 
-#include "wb_master.h"
+#include "wb_master_module.h"
 
 #define BUFFER_SIZE 512
 
 namespace soclib { namespace caba {
 
-	template<typename wb_param>
-		class VideoInModule
-		{
-		public:
+    template<typename wb_param>
+        class VideoInModule: sc_core::sc_module
+    {
+        public:
+            sc_core::sc_in<bool> p_clk;
+            sc_core::sc_in<bool> p_resetn;
+            soclib::caba::WbMaster<wb_param> p_wb;
 
-		//IO PORTS (Video_gen interface)
-			sc_in_clk clk_pix;
-			sc_in<bool> reset_n;
+	    sc_core::sc_in<bool> p_clk_pix;
+	    sc_core::sc_in<bool> reset_n;
+	    sc_core::sc_in<bool> line_valid;
+	    sc_core::sc_in<bool> frame_valid;
+	    sc_core::sc_in<unsigned char> pixel_in;
 
-			sc_in<bool> line_valid;
-			sc_in<bool> frame_valid;
-			sc_in<unsigned char> pixel_in;
+        private:
+            // main thread
+            void process();
+            sc_fifo<unsigned char>* pix_fifo;
+	    long pos;
+	    long offset;
+	    bool write;
+	    bool end_frame;
 
-		////////////////////////////////////////////////////////////////////
-		// single read/write operation
-		////////////////////////////////////////////////////////////////////
-				uint32_t wb_read_at  (
-						uint32_t addr
-						);
+            void     wait_cycles (uint32_t delay);
+            WbMasterModule<wb_param> master0;
 
-				void     wb_write_at (
-						uint32_t addr,
-						uint8_t mask,
-						uint32_t data
-						);
+	    void clk_pix_event();
+	    void clk_wb_event();
 
-		////////////////////////////////////////////////////////////////////
-		// burst read/write operation to/from an uint32_t array
-		////////////////////////////////////////////////////////////////////
-				void     wb_read_blk (
-						uint32_t saddr,
-						uint32_t num ,
-						uint32_t *dest
-						);
+        protected:
+            SC_HAS_PROCESS(VideoInModule);
 
-				void     wb_write_blk (
-						uint32_t saddr,
-						uint32_t *data,
-						uint32_t num
-						);
-
-				void reset ();
-				void print_stats();
-
-				// constructor
-				VideoInModule ( sc_core::sc_in<bool> &p_clk,
-						soclib::caba::WbMaster<wb_param> &p_wb
-					      );
-
-				friend std::ostream& operator<< (std::ostream &o, VideoInModule &wbm)
-				{
-					wbm._print(o);
-					return o;
-				}
-
-			private:
-
-
-		//Buffer
-				unsigned char Buffer[BUFFER_SIZE];
-				long pos;
-				long offset;
-				bool write;
-				bool end_frame;
-
-		// port are private and should be provided  as constructor arguments
-				sc_core::sc_in<bool> &p_clk;
-				soclib::caba::WbMaster<wb_param> &p_wb;
-
-		// simulation cycle
-				uint32_t trans_wait_cycles;
-				uint32_t num_writes;
-				uint32_t num_reads;
-
-		//Pixel rec fnct
-				void clk_pix_event();
-
-		//WB snd fnct
-				void clk_wb_event();
-
-		// wait WB ACK
-				inline void     WaitWbAck()
-				{
-					do {
-						trans_wait_cycles ++;
-						sc_core::wait(p_clk.posedge_event());
-				// error in cycle
-						if (p_wb.ERR_I) {
-							std::cout
-						//<< name()
-								<< " : Error in WB transaction @ cycle #"
-								<< sc_core::sc_time_stamp()
-								<< std::endl;
-							break;
-						}
-
-					} while (!p_wb.ACK_I);
-
-#ifdef SOCLIB_MODULE_DEBUG
-					std::cout
-				//<< name()
-						<< " ACK received @"
-						<< sc_core::sc_time_stamp()
-						<< std::endl;
-#endif
-
-				};
-
-		// Clean WB requests
-				inline void     CleanWb()
-				{
-					p_wb.STB_O  = false;
-					p_wb.CYC_O  = false;
-					p_wb.LOCK_O = false;
-					p_wb.WE_O   = false;
-					p_wb.ADR_O  = 0xBadeC0de;
-					p_wb.DAT_O  = 0xDeadBeef;
-					p_wb.SEL_O  = 0;
-				};
-
-				inline void _print (std::ostream &o)
-				{
-					o
-						<< std::dec
-						<< "Wait cycles " << trans_wait_cycles
-						<< std::endl
-						<< " for " << num_writes << " writes and "
-						<< num_reads << " reads";
-				}
-
-
-				SC_HAS_PROCESS(VideoInModule);
-
-		};
+        public:
+            // constructor
+            VideoInModule ( sc_module_name name );
+    };
 }}
 
-
-#endif //_VIDEO_IN_MODULE_
+#endif //__MASTER__
