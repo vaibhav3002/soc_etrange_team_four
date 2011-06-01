ae5391af0607566db0a0df4ae724d1f9a808eaaf
Thibault Porteboeuf <thibault.porteboeuf@telecom-paristech.fr>
2011-06-01 09:50:02 +0200

    Working UART IRQ

--- SoC/SoC/soft/main.c
+++ SoC/SoC/soft/main.c
@@ -29,69 +29,89 @@
 
 #include <stdio.h>
 #include "lm32_sys.h"
-
+#include "../segmentation.h"
 
 #define N 10
 #define WB_TARGET 0xA1000000
 
+
+extern char inbyte(void);
+
+
 int fibo(int n);
+
+volatile unsigned long cnt=0;
+
+void UART_IrqHandler() {
+	cnt ++;
+	printf("UART CHARACTER RECV +%c+\n",inbyte());
+}
 
 int main(void)
 {
 
-    printf("Read video_in 0x%x\n",*((volatile unsigned int*) 0xA2000000));
-    printf("Write OxAB... Return value: ");
-    *((volatile unsigned int*) 0xA2000000) = 0xAB;
-  
-    printf("0x%x\n",*((volatile unsigned int*) 0xA2000000));
-    printf("Read video_in 0x%x\n",*((volatile unsigned int*) 0xA2000000));
 
-    getchar();
+	//Test interrupt UART
+	RegisterIrqEntry(0,UART_IrqHandler);
+	//Enable global interrutps
+	unsigned long mask = 1;
+	asm volatile("wcsr IE,%0" ::"r"(mask));
+	
+	printf("Read video_in 0x%x\n",*((volatile unsigned int*) 0xA2000000));
+	printf("Write OxAB... Return value: ");
+	*((volatile unsigned int*) 0xA2000000) = 0xAB;
 
-    return 0;
-
-    //mfixed A,B,C,D;
-    int i;
-    int  fibov[N], fibot[N];
-    fibot[0] = get_cc();
-    for (i = 1; i < N; i++)
-    {
-        fibov[i] = fibo(i);fibot[i] = get_cc();
-    }
-    fibov [0]= get_cc();
+	printf("0x%x\n",*((volatile unsigned int*) 0xA2000000));
+	printf("Read video_in 0x%x\n",*((volatile unsigned int*) 0xA2000000));
 
 
-    for (i = 1; i < 10; i++)
-        printf(" Fibo %d : %d at %d\n",i,fibov[i],fibot[i]-fibot[i-1]);
 
-    printf("End  time %d\n\n", fibov[0]);
+	while (cnt<5) {}
 
-    printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
-    printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
-    printf("\nSending write request to simple_slave...\n");
-    *((volatile unsigned int*) WB_TARGET) = 123;
-    printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
-    printf("\nSending write request to simple_slave...\n");
-    *((volatile unsigned int*) WB_TARGET) = 123;
-    printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
-    printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
-    printf("\nSending write request to simple_slave...\n");
-    *((volatile unsigned int*) WB_TARGET) = 123;
-    printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
+	return 0;
 
-    printf("\nDone.\n");
+	//mfixed A,B,C,D;
+	int i;
+	int  fibov[N], fibot[N];
+	fibot[0] = get_cc();
+	for (i = 1; i < N; i++)
+	{
+		fibov[i] = fibo(i);fibot[i] = get_cc();
+	}
+	fibov [0]= get_cc();
 
-    getchar();
-    return 0;
+
+	for (i = 1; i < 10; i++)
+		printf(" Fibo %d : %d at %d\n",i,fibov[i],fibot[i]-fibot[i-1]);
+
+	printf("End  time %d\n\n", fibov[0]);
+
+	printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
+	printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
+	printf("\nSending write request to simple_slave...\n");
+	*((volatile unsigned int*) WB_TARGET) = 123;
+	printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
+	printf("\nSending write request to simple_slave...\n");
+	*((volatile unsigned int*) WB_TARGET) = 123;
+	printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
+	printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
+	printf("\nSending write request to simple_slave...\n");
+	*((volatile unsigned int*) WB_TARGET) = 123;
+	printf("Number of correct received requests: %d\n",*((volatile unsigned int*) WB_TARGET));
+
+	printf("\nDone.\n");
+
+	getchar();
+	return 0;
 }
 
 
 int fibo(int n)
 {
-    if (n==0)
-        return 1;
-    else if (n==1)
-        return 1;
-    else return fibo(n-1) + fibo(n-2);
+	if (n==0)
+		return 1;
+	else if (n==1)
+		return 1;
+	else return fibo(n-1) + fibo(n-2);
 }
 
